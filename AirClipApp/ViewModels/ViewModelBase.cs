b6c0c817851837805939535dd6e72679881a915c
Rodrigo <rodrigo@vancouver4us.com>
2025-02-17 14:40:10 -0800

    Initial commit

---
+++ AirClipApp/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace AirClipApp.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ AirClipApp/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace AirClipApp.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
